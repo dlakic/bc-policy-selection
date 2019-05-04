eff8ec4487ec904f228b7a257b305b9db662db13
Daniel Lakic <7725372+dlakic@users.noreply.github.com>
2019-05-04 17:56:19 +0200

    Add bar chart for costs

--- public/js/policy-list.js
+++ public/js/policy-list.js
@@ -18,3 +18,29 @@ deletePolicyElements.forEach(function (element) {
         deletePolicy(element.value);
     }, false);
 });
+
+const ctx = document.getElementById('costChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ['daily', 'weekly', 'monthly', 'monthly'],
+        datasets: [{
+            label: 'Accumulated costs',
+            backgroundColor: 'rgb(32,156,238)',
+            borderColor: 'rgb(32,156,238)',
+            data: [
+                parseFloat(document.getElementById('dailyCost').textContent),
+                parseFloat(document.getElementById('weeklyCost').textContent),
+                parseFloat(document.getElementById('monthlyCost').textContent),
+                parseFloat(document.getElementById('yearlyCost').textContent),
+
+            ]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
