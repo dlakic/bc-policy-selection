21bed5e9f7e09dd0368f2f4d1ad5527d8513f321
Daniel Lakic <7725372+dlakic@users.noreply.github.com>
2019-05-08 16:11:02 +0200

    Add padding to stats and split into different columns

--- public/js/policy-list.js
+++ public/js/policy-list.js
@@ -5,6 +5,10 @@ function deletePolicy(id) {
         .end((err, res) => {
             if (err) {
                 console.log(err);
+                const errorDiv = document.querySelector('#error');
+                errorDiv.innerHTML = 'ERROR: ' + res.body.message;
+                window.scrollTo(0, 0);
+                return errorDiv.style.display = 'block';
             } else {
                 console.log(res);
                 window.location.replace("/policies/" + res.body.username);
