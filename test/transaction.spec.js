870b8e22b1dea31be941aba86ebb7fba9e631ec9
Daniel Lakic <7725372+dlakic@users.noreply.github.com>
2019-05-25 13:32:35 +0200

    modify test script to show transaction number

--- test/transaction.spec.js
+++ test/transaction.spec.js
@@ -4,18 +4,18 @@ const app = require('../app');
 const username = 'Test';
 const minTemp = 15;
 const maxTemp = 23;
-const iterations = 10;
+const iterations = 1000;
 
-describe('Transaction', () => {
-    for (let i = 1; i <= iterations; i++){
-        it('should make transactions', (done) =>{
-            supertest(app)
-                .post('/api/transactions')
-                .field('username', username)
-                .field('minTemp', minTemp)
-                .field('maxTemp', maxTemp)
-                .attach('xlsxFile', 'sample-data/random15-20.xlsx')
-                .expect(201, done)
-        })
+describe('Transaction', function()  {
+    for (let i = 1; i <= iterations; i++) {
+        it(`should make transactions: ${i}`, (done) => {
+                supertest(app)
+                    .post('/api/transactions')
+                    .field('username', username)
+                    .field('minTemp', minTemp)
+                    .field('maxTemp', maxTemp)
+                    .attach('xlsxFile', 'sample-data/random15-20.xlsx')
+                    .expect(201, done)
+            });
     }
 });
